cf571e4ad701242495fb7522f3c71bb3a2283bd4
dauntless7 <nicolai.8293@gmail.com>
2024-02-08 17:56:58 -0400

    update dependencies

--- src/structures/Client/events/interactionCreate/index.ts
+++ src/structures/Client/events/interactionCreate/index.ts
@@ -1,3 +1,4 @@
+import { info } from '@utils';
 import type { Interaction } from 'discord.js';
 import Event from '../Event';
 import * as handlers from './handlers';
@@ -5,13 +6,17 @@ import * as handlers from './handlers';
 export const onInteraction: Event = {
   packetName: 'interactionCreate',
   async handler(interaction: Interaction) {
-    if (!interaction.isCommand()) {
-      return null;
+    try {
+      if (!interaction?.isCommand()) {
+        return null;
+      }
+      const handler = handlers[interaction.commandName];
+      if (!handler) {
+        return null;
+      }
+      await handler.call(this, interaction);
+    } catch (err) {
+      info(err.message);
     }
-    const handler = handlers[interaction.commandName];
-    if (!handler) {
-      return null;
-    }
-    await handler.call(this, interaction);
   }
 };
